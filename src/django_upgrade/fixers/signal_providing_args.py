d40f7558d49d8a658cf63813317144c11efbbfc1
Adam Johnson <me@adamj.eu>
2021-08-28 23:11:58 +0100

    Add fixer for Django 3.1 Signal argument providing_args

---
+++ src/django_upgrade/fixers/signal_providing_args.py
@@ -0,0 +1,81 @@
+"""
+Remove the 'providing_args' argument from Signal():
+https://docs.djangoproject.com/en/3.1/releases/3.1/#features-deprecated-in-3-1
+"""
+import ast
+from functools import partial
+from typing import Iterable, List, Tuple
+
+from tokenize_rt import UNIMPORTANT_WS, Offset, Token
+
+from django_upgrade.ast import ast_start_offset
+from django_upgrade.data import Fixer, State, TokenFunc
+from django_upgrade.tokens import (
+    CODE,
+    COMMENT,
+    INDENT,
+    OP,
+    consume,
+    find,
+    parse_call_args,
+    reverse_consume,
+)
+
+fixer = Fixer(
+    __name__,
+    min_version=(3, 1),
+)
+
+MODULE = "django.dispatch"
+NAME = "Signal"
+
+
+@fixer.register(ast.Call)
+def visit_Call(
+    state: State,
+    node: ast.Call,
+    parent: ast.AST,
+) -> Iterable[Tuple[Offset, TokenFunc]]:
+    if (
+        NAME in state.from_imports[MODULE]
+        and isinstance(node.func, ast.Name)
+        and node.func.id == NAME
+        and (
+            len(node.args) > 0 or any(k.arg == "providing_args" for k in node.keywords)
+        )
+    ):
+        yield ast_start_offset(node), partial(
+            remove_providing_args,
+            node=node,
+        )
+
+
+def remove_providing_args(tokens: List[Token], i: int, *, node: ast.Call) -> None:
+    j = find(tokens, i, name=OP, src="(")
+    func_args, _ = parse_call_args(tokens, j)
+
+    if len(node.args):
+        start_idx, end_idx = func_args[0]
+        if len(node.args) == 1:
+            del tokens[start_idx:end_idx]
+        else:
+            # Have to replace with None
+            tokens[start_idx:end_idx] = [Token(name=CODE, src="None")]
+    else:
+        for n, keyword in enumerate(node.keywords):
+            if keyword.arg == "providing_args":
+                start_idx, end_idx = func_args[n]
+
+                start_idx = reverse_consume(tokens, start_idx, name=UNIMPORTANT_WS)
+                start_idx = reverse_consume(tokens, start_idx, name=INDENT)
+                if n > 0:
+                    start_idx = reverse_consume(tokens, start_idx, name=OP, src=",")
+
+                if n < len(node.keywords) - 1:
+                    end_idx = consume(tokens, end_idx, name=UNIMPORTANT_WS)
+                    end_idx = consume(tokens, end_idx, name=OP, src=",")
+                    end_idx = consume(tokens, end_idx, name=UNIMPORTANT_WS)
+                    end_idx = consume(tokens, end_idx, name=COMMENT)
+                    end_idx += 1
+
+                del tokens[start_idx:end_idx]
---
+++ tests/fixers/test_signal_providing_args.py
@@ -0,0 +1,156 @@
+from django_upgrade.data import Settings
+from tests.fixers.tools import check_noop, check_transformed
+
+settings = Settings(target_version=(3, 1))
+
+
+def test_no_deprecated_arg():
+    check_noop(
+        """\
+        from django.dispatch import Signal
+        Signal(use_caching=True)
+        """,
+        settings,
+    )
+
+
+def test_pos_arg_alone():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        Signal(["documented", "arg"])
+        """,
+        """\
+        from django.dispatch import Signal
+        Signal()
+        """,
+        settings,
+    )
+
+
+def test_pos_arg_alone_multiline():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        my_signal = Signal(
+            [
+                "documented", "arg"
+                             ])
+        """,
+        """\
+        from django.dispatch import Signal
+        my_signal = Signal()
+        """,
+        settings,
+    )
+
+
+def test_pos_arg_with_caching():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        Signal(["documented", "arg"], True)
+        """,
+        """\
+        from django.dispatch import Signal
+        Signal(None, True)
+        """,
+        settings,
+    )
+
+
+def test_kwarg_alone():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        Signal(providing_args=["documented", "arg"])
+        """,
+        """\
+        from django.dispatch import Signal
+        Signal()
+        """,
+        settings,
+    )
+
+
+def test_kwarg_with_caching():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        Signal(providing_args=["documented", "arg"], use_caching=True)
+        """,
+        """\
+        from django.dispatch import Signal
+        Signal(use_caching=True)
+        """,
+        settings,
+    )
+
+
+def test_kwarg_with_caching_no_space():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        Signal(providing_args=["documented", "arg"],use_caching=True)
+        """,
+        """\
+        from django.dispatch import Signal
+        Signal(use_caching=True)
+        """,
+        settings,
+    )
+
+
+def test_kwarg_with_caching_reordered():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        Signal(use_caching=True, providing_args=["documented", "arg"])
+        """,
+        """\
+        from django.dispatch import Signal
+        Signal(use_caching=True)
+        """,
+        settings,
+    )
+
+
+def test_kwarg_with_caching_multiline():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        Signal(
+            providing_args=["documented", "arg"],
+            use_caching=True,
+        )
+        """,
+        """\
+        from django.dispatch import Signal
+        Signal(
+            use_caching=True,
+        )
+        """,
+        settings,
+    )
+
+
+def test_kwarg_with_all_extras():
+    check_transformed(
+        """\
+        from django.dispatch import Signal
+        Signal(
+              providing_args=[
+                "documented",
+                        "arg",
+            ]  ,  # documents the arguments
+            use_caching=True,
+        )
+        """,
+        """\
+        from django.dispatch import Signal
+        Signal(
+            use_caching=True,
+        )
+        """,
+        settings,
+    )

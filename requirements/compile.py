540c1066f7c76752bb4c84e74348d5d7ab7a0bd8
Adam Johnson <me@adamj.eu>
2021-08-28 14:59:26 +0100

    Trim Python versions (#13)

--- requirements/compile.py
+++ requirements/compile.py
@@ -10,14 +10,6 @@ if __name__ == "__main__":
     os.environ.pop("PIP_REQUIRE_VIRTUALENV")
     common_args = ["-m", "piptools", "compile", "--generate-hashes"] + sys.argv[1:]
     subprocess.run(
-        ["python3.6", *common_args, "-o", "py36.txt"],
-        check=True,
-    )
-    subprocess.run(
-        ["python3.7", *common_args, "-o", "py37.txt"],
-        check=True,
-    )
-    subprocess.run(
         ["python3.8", *common_args, "-o", "py38.txt"],
         check=True,
     )

5de673ab84350756e3287067c948ff8123a6ba6c
Adam Johnson <me@adamj.eu>
2021-08-30 09:48:28 +0100

    Add fixer for rename of FloatRangeField to DecimalRangeField

---
+++ src/django_upgrade/fixers/postgres_float_range_field.py
@@ -0,0 +1,57 @@
+"""
+Replace FloatRangeField with DecimalRangeField:
+https://docs.djangoproject.com/en/2.2/releases/2.2/#features-deprecated-in-2-2
+"""
+import ast
+from functools import partial
+from typing import Iterable, Tuple
+
+from tokenize_rt import Offset
+
+from django_upgrade.ast import ast_start_offset
+from django_upgrade.data import Fixer, State, TokenFunc
+from django_upgrade.tokens import find_and_replace_name, update_imports
+
+fixer = Fixer(
+    __name__,
+    min_version=(2, 2),
+)
+
+MODULES = {
+    "django.contrib.postgres.fields",
+    "django.contrib.postgres.fields.ranges",
+    "django.contrib.postgres.forms",
+    "django.contrib.postgres.forms.ranges",
+}
+NAME_MAP = {
+    "FloatRangeField": "DecimalRangeField",
+}
+
+
+@fixer.register(ast.ImportFrom)
+def visit_ImportFrom(
+    state: State,
+    node: ast.ImportFrom,
+    parent: ast.AST,
+) -> Iterable[Tuple[Offset, TokenFunc]]:
+    if (
+        node.level == 0
+        and node.module in MODULES
+        and any(alias.name in NAME_MAP for alias in node.names)
+    ):
+        yield ast_start_offset(node), partial(
+            update_imports, node=node, name_map=NAME_MAP
+        )
+
+
+@fixer.register(ast.Name)
+def visit_Name(
+    state: State,
+    node: ast.Name,
+    parent: ast.AST,
+) -> Iterable[Tuple[Offset, TokenFunc]]:
+    name = node.id
+    if name in NAME_MAP and any(name in state.from_imports[m] for m in MODULES):
+        yield ast_start_offset(node), partial(
+            find_and_replace_name, name=name, new=NAME_MAP[name]
+        )
---
+++ tests/fixers/test_postgres_float_range_field.py
@@ -0,0 +1,51 @@
+from django_upgrade.data import Settings
+from tests.fixers.tools import check_noop, check_transformed
+
+settings = Settings(target_version=(2, 2))
+
+
+def test_no_deprecated_alias():
+    check_noop(
+        """\
+        from django.contrib.postgres.fields import ArrayField
+
+        ArrayField
+        """,
+        settings,
+    )
+
+
+def test_direct_import():
+    check_transformed(
+        """\
+        from django.db.models import Model
+        from django.contrib.postgres.fields import FloatRangeField
+
+        class MyModel(Model):
+            my_field = FloatRangeField("My range of numbers")
+        """,
+        """\
+        from django.db.models import Model
+        from django.contrib.postgres.fields import DecimalRangeField
+
+        class MyModel(Model):
+            my_field = DecimalRangeField("My range of numbers")
+        """,
+        settings,
+    )
+
+
+def test_success_alias():
+    check_transformed(
+        """\
+        from django.contrib.postgres.forms.ranges import FloatRangeField as FRF
+
+        FRF("yada")
+        """,
+        """\
+        from django.contrib.postgres.forms.ranges import DecimalRangeField as FRF
+
+        FRF("yada")
+        """,
+        settings,
+    )

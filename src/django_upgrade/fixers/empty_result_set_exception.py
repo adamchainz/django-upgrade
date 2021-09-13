695dc10382a93e213bfb2c9d868514c7f8dc0ba1
Bruno Alla <alla.brunoo@gmail.com>
2021-09-13 20:16:17 +0100

    Refactor function to rewrite imports from a dict mapping

--- src/django_upgrade/fixers/empty_result_set_exception.py
+++ src/django_upgrade/fixers/empty_result_set_exception.py
@@ -3,15 +3,14 @@ Replace compatibility imports for django.core.exceptions.EmptyResultSet:
 https://docs.djangoproject.com/en/3.1/releases/3.1/#id1
 """
 import ast
-from collections import defaultdict
 from functools import partial
-from typing import Iterable, List, Tuple
+from typing import Iterable, Tuple
 
-from tokenize_rt import Offset, Token
+from tokenize_rt import Offset
 
 from django_upgrade.ast import ast_start_offset
 from django_upgrade.data import Fixer, State, TokenFunc
-from django_upgrade.tokens import extract_indent, insert, update_imports
+from django_upgrade.tokens import rewrite_imports
 
 fixer = Fixer(
     __name__,
@@ -42,23 +41,6 @@ def visit_ImportFrom(
         and node.module in REWRITES
         and any(alias.name in REWRITES[node.module] for alias in node.names)
     ):
-        yield ast_start_offset(node), partial(fix_import, node=node)
-
-
-def fix_import(tokens: List[Token], i: int, *, node: ast.ImportFrom) -> None:
-    assert node.module is not None
-    module_rewrites = REWRITES[node.module]
-    imports_to_add = defaultdict(list)
-    name_map = {}
-    for alias in node.names:
-        name = alias.name
-        if name in module_rewrites:
-            name_map[name] = ""
-            new_name = f"{name} as {alias.asname}" if alias.asname else name
-            imports_to_add[module_rewrites[name]].append(new_name)
-
-    j, indent = extract_indent(tokens, i)
-    update_imports(tokens, i, node=node, name_map=name_map)
-    for module, names in reversed(imports_to_add.items()):
-        joined_names = ", ".join(sorted(names))
-        insert(tokens, j, new_src=f"{indent}from {module} import {joined_names}\n")
+        yield ast_start_offset(node), partial(
+            rewrite_imports, node=node, rewrites=REWRITES
+        )

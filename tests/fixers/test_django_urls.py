385e41c8939f67e8144f27d811f2b735a4781da5
Adam Johnson <me@adamj.eu>
2022-08-25 13:28:37 +0100

    finish de-parameterizing tests

--- tests/fixers/test_django_urls.py
+++ tests/fixers/test_django_urls.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import pytest
-
 from django_upgrade.data import Settings
 from tests.fixers.tools import check_noop, check_transformed
 
@@ -401,272 +399,305 @@ def test_complete():
     )
 
 
-@pytest.mark.parametrize(
-    "before, after",
-    [
-        pytest.param(
-            """\
-            from django.urls import re_path
+def test_re_path_empty():
+    check_transformed(
+        """\
+        from django.urls import re_path
 
-            re_path(r"^$", views.index)
-            """,
-            """\
-            from django.urls import path
+        re_path(r"^$", views.index)
+        """,
+        """\
+        from django.urls import path
 
-            path('', views.index)
-            """,
-            id="path_empty",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path
-
-            re_path(r'^about/$', views.about, name='about')
-            """,
-            """\
-            from django.urls import path
-
-            path('about/', views.about, name='about')
-            """,
-            id="path_simple",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path
-
-            re_path(r'about/$', views.about)
-            """,
-            """\
-            from django.urls import path
-
-            path('about/', views.about)
-            """,
-            id="path_unanchored_start",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path
-
-            re_path(r'^more-info/$', views.more_info)
-            """,
-            """\
-            from django.urls import path
-
-            path('more-info/', views.more_info)
-            """,
-            id="path_with_dash",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path, reverse
-
-            re_path(r'^more-info/$', views.more_info)
-            """,
-            """\
-            from django.urls import path, reverse
-
-            path('more-info/', views.more_info)
-            """,
-            id="multiple_import",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path
-
-            re_path(r'^page/(?P<number>[0-9]+)/$', views.page)
-            """,
-            """\
-            from django.urls import path
-
-            path('page/<int:number>/', views.page)
-            """,
-            id="path_int_converter_1",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path
-
-            re_path(r'^page/(?P<number>\\d+)/$', views.page)
-            """,
-            """\
-            from django.urls import path
-
-            path('page/<int:number>/', views.page)
-            """,
-            id="path_int_converter_2",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path
-
-            re_path(r'^(?P<subpath>.+)/$', views.default)
-            """,
-            """\
-            from django.urls import path
-
-            path('<path:subpath>/', views.default)
-            """,
-            id="path_path_converter",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path
-
-            re_path(r'^post/(?P<slug>[-a-zA-Z0-9_]+)/$', views.post)
-            """,
-            """\
-            from django.urls import path
-
-            path('post/<slug:slug>/', views.post)
-            """,
-            id="path_slug_converter",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path
-
-            re_path(r'^uuid/(?P<uuid>{})/$', by_uuid)
-            """.format(
-                "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
-            ),
-            """\
-            from django.urls import path
-
-            path('uuid/<uuid:uuid>/', by_uuid)
-            """,
-            id="path_str_converter",
-        ),
-        pytest.param(
-            """\
-            from django.urls import include, re_path
-
-            urlpatterns = [
-                re_path(r'^$', views.index, name='index'),
-                re_path(r'^about/$', views.about, name='about'),
-                re_path(r'^post/(?P<slug>[-a-zA-Z0-9_]+)/$', views.post, name='post'),
-                re_path(r'^post/(?P<year>[0-9]{4})/$', views.post, name='post'),
-                re_path(r'^weblog/', include('blog.urls')),
-            ]
-            """,
-            """\
-            from django.urls import include, path, re_path
-
-            urlpatterns = [
-                path('', views.index, name='index'),
-                path('about/', views.about, name='about'),
-                path('post/<slug:slug>/', views.post, name='post'),
-                re_path(r'^post/(?P<year>[0-9]{4})/$', views.post, name='post'),
-                re_path(r'^weblog/', include('blog.urls')),
-            ]
-            """,
-            id="re_path_test_complete",
-        ),
-    ],
-)
-def test_re_path_transform(before: str, after: str) -> None:
-    check_transformed(before, after, settings=settings)
+        path('', views.index)
+        """,
+        settings,
+    )
 
 
-@pytest.mark.parametrize(
-    "before, after",
-    [
-        pytest.param(
-            """\
-            from django.conf.urls import include
-            from django.urls import re_path, reverse
+def test_re_path_simple():
+    check_transformed(
+        """\
+        from django.urls import re_path
 
-            urlpatterns = [
-                re_path(r'^$', views.index, name='index'),
-                re_path(r'^weblog/', include('blog.urls')),
-            ]
-            """,
-            """\
-            from django.urls import include, path, re_path, reverse
+        re_path(r'^about/$', views.about, name='about')
+        """,
+        """\
+        from django.urls import path
 
-            urlpatterns = [
-                path('', views.index, name='index'),
-                re_path(r'^weblog/', include('blog.urls')),
-            ]
-            """,
-            id="django.conf import first - keep re_path",
-        ),
-        pytest.param(
-            """\
-            from django.conf.urls import url, include
-            from django.urls import re_path, reverse
+        path('about/', views.about, name='about')
+        """,
+        settings,
+    )
 
-            urlpatterns = [
-                url(r'^$', views.index, name='index'),
-                re_path(r'^weblog/$', include('blog.urls')),
-            ]
-            """,
-            """\
-            from django.urls import include, path, reverse
 
-            urlpatterns = [
-                path('', views.index, name='index'),
-                path('weblog/', include('blog.urls')),
-            ]
-            """,
-            id="django.conf import first - remove re_path",
-        ),
-        pytest.param(
-            """\
-            from django.conf.urls import include
-            from django.urls import re_path, reverse
+def test_re_path_unanchored_start():
+    check_transformed(
+        """\
+        from django.urls import re_path
 
-            urlpatterns = [
-                re_path(r'^$', views.index, name='index'),
-                re_path(r'^weblog/', include('blog.urls')),
-            ]
-            """,
-            """\
-            from django.urls import include, path, re_path, reverse
+        re_path(r'about/$', views.about)
+        """,
+        """\
+        from django.urls import path
 
-            urlpatterns = [
-                path('', views.index, name='index'),
-                re_path(r'^weblog/', include('blog.urls')),
-            ]
-            """,
-            id="django.conf import second - keep re_path",
-        ),
-        pytest.param(
-            """\
-            from django.urls import re_path, reverse
-            from django.conf.urls import url, include
+        path('about/', views.about)
+        """,
+        settings,
+    )
 
-            urlpatterns = [
-                url(r'^$', views.index, name='index'),
-                re_path(r'^weblog/$', include('blog.urls')),
-            ]
-            """,
-            """\
-            from django.urls import include, path, reverse
 
-            urlpatterns = [
-                path('', views.index, name='index'),
-                path('weblog/', include('blog.urls')),
-            ]
-            """,
-            id="django.conf import second - remove re_path",
-        ),
-        pytest.param(
-            """\
-            from django.conf.urls import include
-            from django.urls import re_path
+def test_re_path_multiple_import():
+    check_transformed(
+        """\
+        from django.urls import re_path, reverse
 
-            include('example.urls')
-            re_path(r"^$", views.index)
-            """,
-            """\
-            from django.urls import include, path
+        re_path(r'^more-info/$', views.more_info)
+        """,
+        """\
+        from django.urls import path, reverse
 
-            include('example.urls')
-            path('', views.index)
-            """,
-            id="Test only include from django.conf.urls",
-        ),
-    ],
-)
-def test_concurrent_transform(before: str, after: str) -> None:
-    check_transformed(before, after, settings)
+        path('more-info/', views.more_info)
+        """,
+        settings,
+    )
+
+
+def test_re_path_int_converter_1():
+    check_transformed(
+        """\
+        from django.urls import re_path
+
+        re_path(r'^page/(?P<number>[0-9]+)/$', views.page)
+        """,
+        """\
+        from django.urls import path
+
+        path('page/<int:number>/', views.page)
+        """,
+        settings,
+    )
+
+
+def test_re_path_int_converter_2():
+    check_transformed(
+        """\
+        from django.urls import re_path
+
+        re_path(r'^page/(?P<number>\\d+)/$', views.page)
+        """,
+        """\
+        from django.urls import path
+
+        path('page/<int:number>/', views.page)
+        """,
+        settings,
+    )
+
+
+def test_re_path_path_converter():
+    check_transformed(
+        """\
+        from django.urls import re_path
+
+        re_path(r'^(?P<subpath>.+)/$', views.default)
+        """,
+        """\
+        from django.urls import path
+
+        path('<path:subpath>/', views.default)
+        """,
+        settings,
+    )
+
+
+def test_re_path_slug_converter():
+    check_transformed(
+        """\
+        from django.urls import re_path
+
+        re_path(r'^post/(?P<slug>[-a-zA-Z0-9_]+)/$', views.post)
+        """,
+        """\
+        from django.urls import path
+
+        path('post/<slug:slug>/', views.post)
+        """,
+        settings,
+    )
+
+
+def test_re_path_str_converter():
+    check_transformed(
+        """\
+        from django.urls import re_path
+
+        re_path(r'^about/(?P<name>[^/]+)/$', views.about)
+        """,
+        """\
+        from django.urls import path
+
+        path('about/<str:name>/', views.about)
+        """,
+        settings,
+    )
+
+
+def test_re_path_uuid_converter():
+    uuid_re = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
+    check_transformed(
+        f"""\
+        from django.urls import re_path
+
+        re_path(r'^uuid/(?P<uuid>{uuid_re})/$', by_uuid)
+        """,
+        """\
+        from django.urls import path
+
+        path('uuid/<uuid:uuid>/', by_uuid)
+        """,
+        settings,
+    )
+
+
+def test_re_path_complete():
+    check_transformed(
+        """\
+        from django.urls import include, re_path
+
+        urlpatterns = [
+            re_path(r'^$', views.index, name='index'),
+            re_path(r'^about/$', views.about, name='about'),
+            re_path(r'^post/(?P<slug>[-a-zA-Z0-9_]+)/$', views.post, name='post'),
+            re_path(r'^post/(?P<year>[0-9]{4})/$', views.post, name='post'),
+            re_path(r'^weblog/', include('blog.urls')),
+        ]
+        """,
+        """\
+        from django.urls import include, path, re_path
+
+        urlpatterns = [
+            path('', views.index, name='index'),
+            path('about/', views.about, name='about'),
+            path('post/<slug:slug>/', views.post, name='post'),
+            re_path(r'^post/(?P<year>[0-9]{4})/$', views.post, name='post'),
+            re_path(r'^weblog/', include('blog.urls')),
+        ]
+        """,
+        settings,
+    )
+
+
+def test_combined_keep_re_path():
+    # conf import for url() not procesed, so re_path's do not get converted
+    check_transformed(
+        """\
+        from django.conf.urls import include
+        from django.urls import re_path, reverse
+
+        urlpatterns = [
+            re_path(r'^$', views.index, name='index'),
+            re_path(r'^weblog/', include('blog.urls')),
+        ]
+        """,
+        """\
+        from django.urls import include, path, re_path, reverse
+
+        urlpatterns = [
+            path('', views.index, name='index'),
+            re_path(r'^weblog/', include('blog.urls')),
+        ]
+        """,
+        settings,
+    )
+
+
+def test_combined_rewrite_all():
+    # conf import being procesed, so re_path's do get converted
+    check_transformed(
+        """\
+        from django.conf.urls import url, include
+        from django.urls import re_path, reverse
+
+        urlpatterns = [
+            url(r'^$', views.index, name='index'),
+            re_path(r'^weblog/$', include('blog.urls')),
+        ]
+        """,
+        """\
+        from django.urls import include, path, reverse
+
+        urlpatterns = [
+            path('', views.index, name='index'),
+            path('weblog/', include('blog.urls')),
+        ]
+        """,
+        settings,
+    )
+
+
+def test_combined_3():
+    # re_path already imported, so re_path's do not get converted
+    check_transformed(
+        """\
+        from django.conf.urls import include
+        from django.urls import re_path, reverse
+
+        urlpatterns = [
+            re_path(r'^$', views.index, name='index'),
+            re_path(r'^weblog/', include('blog.urls')),
+        ]
+        """,
+        """\
+        from django.urls import include, path, re_path, reverse
+
+        urlpatterns = [
+            path('', views.index, name='index'),
+            re_path(r'^weblog/', include('blog.urls')),
+        ]
+        """,
+        settings,
+    )
+
+
+def test_combined_4():
+    check_transformed(
+        """\
+        from django.urls import re_path, reverse
+        from django.conf.urls import url, include
+
+        urlpatterns = [
+            url(r'^$', views.index, name='index'),
+            re_path(r'^weblog/$', include('blog.urls')),
+        ]
+        """,
+        """\
+        from django.urls import include, path, reverse
+
+        urlpatterns = [
+            path('', views.index, name='index'),
+            path('weblog/', include('blog.urls')),
+        ]
+        """,
+        settings,
+    )
+
+
+def test_combined_5():
+    # only 'include'
+    check_transformed(
+        """\
+        from django.conf.urls import include
+        from django.urls import re_path
+
+        include('example.urls')
+        re_path(r"^$", views.index)
+        """,
+        """\
+        from django.urls import include, path
+
+        include('example.urls')
+        path('', views.index)
+        """,
+        settings,
+    )

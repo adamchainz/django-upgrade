d1f8ef40eb7074804b7e9c5cb28cca0d6df4ed9e
Adam Johnson <me@adamj.eu>
2022-09-13 13:36:33 +0100

    Add test assertion that target code is valid AST

--- tests/fixers/tools.py
+++ tests/fixers/tools.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import ast
 from textwrap import dedent
 
 from django_upgrade.data import Settings
@@ -19,5 +20,6 @@ def check_transformed(
 ) -> None:
     dedented_before = dedent(before)
     dedented_after = dedent(after)
+    ast.parse(dedented_after)  # check that the target is valid python code
     fixed = apply_fixers(dedented_before, settings=settings, filename=filename)
     assert fixed == dedented_after

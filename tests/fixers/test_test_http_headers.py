3efe5a044baf2c3351b99f41c15e0d12a0ff8e60
Adam Johnson <me@adamj.eu>
2023-02-12 11:07:42 +0000

    Add more tests and improve existing ones

--- tests/fixers/test_test_http_headers.py
+++ tests/fixers/test_test_http_headers.py
@@ -7,88 +7,37 @@ from tests.fixers.tools import check_transformed
 settings = Settings(target_version=(4, 2))
 
 
-def test_transform_constructor_call():
-    check_transformed(
+def test_non_test_file():
+    check_noop(
         """\
-        from django.test import RequestFactory
-        RequestFactory(HTTP_HOST="test.com")
+        self.client.get("/", HTTP_HOST="example.com")
         """,
-        """\
-        from django.test import RequestFactory
-        RequestFactory(headers={"host": "test.com"})
+        settings,
+    )
+
+
+def test_custom_client_class_instantiation():
+    check_noop(
+        """
+        from example.test import Client
+        Client(HTTP_HOST="example.com")
         """,
         settings,
         filename="tests.py",
     )
 
 
-def test_transform_constructor_call_multiple():
-    check_transformed(
-        """\
-        from django.test import Client
-        c = Client(HTTP_HOST="test.com", headers={"user-agent": "example"}, HTTP_ACCEPT_LANGUAGE="fr-fr")
-        """,  # noqa: E501
-        """\
-        from django.test import Client
-        c = Client(headers={"user-agent": "example", "host": "test.com", "accept-language": "fr-fr"}, )
-        """,  # noqa: E501
-        settings,
-        filename="tests.py",
-    )
-
-
-def test_transform():
-    check_transformed(
-        """\
-        from django.test import Client
-        self.client = Client()
-        self.client.get("/", HTTP_HOST="test.com", SCRIPT_NAME="/app/")
-        """,
-        """\
-        from django.test import Client
-        self.client = Client()
-        self.client.get("/", headers={"host": "test.com"}, SCRIPT_NAME="/app/")
+def test_custom_client_call():
+    check_noop(
+        """
+        self.custom_client.get("/", HTTP_HOST="example.com"),
         """,
         settings,
         filename="tests.py",
     )
 
 
-def test_transform_only():
-    check_transformed(
-        """\
-        from django.test import Client
-        self.client = Client()
-        self.client.get("/", HTTP_HOST="test.com")
-        """,
-        """\
-        from django.test import Client
-        self.client = Client()
-        self.client.get("/", headers={"host": "test.com"})
-        """,
-        settings,
-        filename="tests.py",
-    )
-
-
-def test_transform_multiple():
-    check_transformed(
-        """\
-        from django.test import Client
-        self.client = Client()
-        self.client.get("/", HTTP_HOST="test.com", HTTP_ACCEPT="application/json")
-        """,
-        """\
-        from django.test import Client
-        self.client = Client()
-        self.client.get("/", headers={"host": "test.com", "accept": "application/json"})
-        """,
-        settings,
-        filename="tests.py",
-    )
-
-
-def test_unchanged_parameter():
+def test_client_call_non_http_kwarg():
     check_noop(
         """
         self.client.get("/", SCRIPT_NAME="/app/"),
@@ -98,31 +47,250 @@ def test_unchanged_parameter():
     )
 
 
-def test_unchanged_other_call():
-    check_noop(
-        """
-        request.META.get("/", HTTP_HOST="host.com"),
+def test_instantiation_request_factory():
+    check_transformed(
+        """\
+        from django.test import RequestFactory
+        RequestFactory(HTTP_HOST="example.com")
+        """,
+        """\
+        from django.test import RequestFactory
+        RequestFactory(headers={"host": "example.com"})
         """,
         settings,
         filename="tests.py",
     )
 
 
-def test_transform_multiline():
+def test_instantiation():
+    check_transformed(
+        """\
+        from django.test import Client
+        Client(HTTP_HOST="example.com")
+        """,
+        """\
+        from django.test import Client
+        Client(headers={"host": "example.com"})
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_instantiation_other_arg():
+    check_transformed(
+        """\
+        from django.test import Client
+        Client(enforce_csrf_checks=False, HTTP_HOST="example.com")
+        """,
+        """\
+        from django.test import Client
+        Client(enforce_csrf_checks=False, headers={"host": "example.com"})
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_instantiation_multiline():
+    check_transformed(
+        """\
+        from django.test import Client
+        Client(
+            HTTP_HOST="example.com"
+        )
+        """,
+        """\
+        from django.test import Client
+        Client(
+            headers={"host": "example.com"
+        })
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_instantiation_multiple():
+    check_transformed(
+        """\
+        from django.test import Client
+        Client(HTTP_A="1", HTTP_B="2")
+        """,
+        """\
+        from django.test import Client
+        Client(headers={"a": "1", "b": "2"})
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_instantiation_multiple_surrounding_existing():
+    check_transformed(
+        """\
+        from django.test import Client
+        Client(HTTP_A="1", headers={"b": "2"}, HTTP_C="3")
+        """,
+        """\
+        from django.test import Client
+        Client(headers={"b": "2", "a": "1", "c": "3"}, )
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_instantiation_multiple_before_existing():
+    check_transformed(
+        """\
+        from django.test import Client
+        Client(HTTP_A="1", HTTP_B="2", headers={"c": "3"})
+        """,
+        """\
+        from django.test import Client
+        Client(headers={"c": "3", "a": "1", "b": "2"})
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_instantiation_multiple_after_existing():
+    check_transformed(
+        """\
+        from django.test import Client
+        Client(headers={"c": "3"}, HTTP_A="1", HTTP_B="2")
+        """,
+        """\
+        from django.test import Client
+        Client(headers={"c": "3", "a": "1", "b": "2"}, )
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_client_call():
+    check_transformed(
+        """\
+        self.client.get("/", HTTP_HOST="example.com")
+        """,
+        """\
+        self.client.get("/", headers={"host": "example.com"})
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_client_call_multiple():
+    check_transformed(
+        """\
+        self.client.get("/", HTTP_HOST="example.com", HTTP_ACCEPT="text/html")
+        """,
+        """\
+        self.client.get("/", headers={"host": "example.com", "accept": "text/html"})
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_client_call_extra_arg():
+    check_transformed(
+        """\
+        self.client.get("/", HTTP_HOST="example.com", SCRIPT_NAME="/app/")
+        """,
+        """\
+        self.client.get("/", headers={"host": "example.com"}, SCRIPT_NAME="/app/")
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_client_call_multiline():
     check_transformed(
         """
-        response = self.client.post(
-            "/password_reset/",
-            {"email": "staffmember@example.com"},
-            HTTP_HOST="www.example:dr.frankenstein@evil.tld",
+        response = self.client.get(
+            "/",
+            {"q": "abc"},
+            HTTP_HOST="example.com",
         )
         """,
         """
-        response = self.client.post(
-            "/password_reset/",
-            {"email": "staffmember@example.com"},
-            headers={"host": "www.example:dr.frankenstein@evil.tld"}
+        response = self.client.get(
+            "/",
+            {"q": "abc"},
+            headers={"host": "example.com"}
         )
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_client_call_multiline_multiple():
+    check_transformed(
+        """
+        response = self.client.get(
+            "/",
+            {"q": "abc"},
+            HTTP_HOST="example.com",
+            HTTP_ACCEPT="text/html",
+        )
+        """,
+        """
+        response = self.client.get(
+            "/",
+            {"q": "abc"},
+            headers={"host": "example.com", "accept": "text/html"}
+
+        )
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_client_variable():
+    check_transformed(
+        """\
+        self.client.get("/", HTTP_HOST=host)
+        """,
+        """\
+        self.client.get("/", headers={"host": host})
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_client_expression():
+    check_transformed(
+        """\
+        self.client.get("/", HTTP_HOST=(name + tld))
+        """,
+        """\
+        self.client.get("/", headers={"host": (name + tld)})
+        """,
+        settings,
+        filename="tests.py",
+    )
+
+
+def test_client_expression_multiline():
+    check_transformed(
+        """\
+        self.client.get("/", HTTP_HOST=(
+            name + tld
+        ))
+        """,
+        """\
+        self.client.get("/", headers={"host": (
+            name + tld
+        )})
         """,
         settings,
         filename="tests.py",

beb936611888d5c306fea2a56843983948291489
Alessandro Ferrini <alessandro.ferrini@apptecsrl.com>
2024-02-06 16:50:54 +0100

    fix issue #419

---
+++ src/django_upgrade/fixers/request_user_is_authenticated .py
@@ -0,0 +1,86 @@
+"""
+Update use of request.user.is_authenticated() to use request.user.is_authenticated 
+https://docs.djangoproject.com/en/1.10/releases/1.10/#using-user-is-authenticated-and-user-is-anonymous-as-methods
+"""
+from __future__ import annotations
+
+import ast
+import sys
+from functools import partial
+from typing import Iterable
+
+from tokenize_rt import Offset
+from tokenize_rt import Token
+
+from django_upgrade.ast import ast_start_offset
+from django_upgrade.data import Fixer
+from django_upgrade.data import State
+from django_upgrade.data import TokenFunc
+from django_upgrade.tokens import find
+from django_upgrade.tokens import NAME
+from django_upgrade.tokens import OP
+
+
+fixer = Fixer(
+    __name__,
+    min_version=(1, 10),
+)
+
+
+@fixer.register(ast.Call)
+def visit_Call(
+    state: State,
+    node: ast.Call,
+    parents: list[ast.AST],
+) -> Iterable[tuple[Offset, TokenFunc]]:
+    if (
+        isinstance(node.func, ast.Attribute)
+        and node.func.attr == "is_authenticated"
+        and is_request_user_or_self_request_user(node.func.value)
+        and len(node.args) == 0
+    ):
+        yield (
+            ast_start_offset(node),
+            partial(rewrite_user_is_auth),
+        )
+
+
+def is_request_user_or_self_request_user(node: ast.AST) -> bool:
+    return (
+        isinstance(node, ast.Attribute)
+        and node.attr == "user"
+        and (
+            (isinstance(node.value, ast.Name) and node.value.id == "request")
+            or (
+                isinstance(node.value, ast.Attribute)
+                and isinstance(node.value.value, ast.Name)
+                and node.value.value.id == "self"
+                and node.value.attr == "request"
+            )
+        )
+    )
+
+
+if sys.version_info >= (3, 9):
+
+    def extract_constant(node: ast.AST) -> str | None:
+        if isinstance(node, ast.Constant) and isinstance(node.value, str):
+            return node.value
+        return None
+
+else:
+
+    def extract_constant(node: ast.AST) -> str | None:
+        if (
+            isinstance(node, ast.Index)
+            and isinstance(node.value, ast.Constant)
+            and isinstance(node.value.value, str)
+        ):
+            return node.value.value
+        return None
+
+
+def rewrite_user_is_auth(tokens: list[Token], i: int) -> None:
+    j = find(tokens, i, name=NAME, src="is_authenticated")
+    y = find(tokens, i, name=OP, src=")")
+    del tokens[j + 1 : y + 1]
---
+++ tests/fixers/test_is_authenticated.py
@@ -0,0 +1,175 @@
+from __future__ import annotations
+
+from django_upgrade.data import Settings
+from tests.fixers.tools import check_noop
+from tests.fixers.tools import check_transformed
+
+settings = Settings(target_version=(1, 10))
+
+
+def test_not_request():
+    check_noop(
+        """\
+        user.is_authenticated()
+        """,
+        settings,
+    )
+
+
+def test_not_self_request():
+    check_noop(
+        """\
+        self.user.is_authenticated()
+        """,
+        settings,
+    )
+
+
+def test_not_user():
+    check_noop(
+        """\
+        request.is_authenticated()
+        """,
+        settings,
+    )
+
+
+def test_not_self_user():
+    check_noop(
+        """\
+        self.request.is_authenticated()
+        """,
+        settings,
+    )
+
+
+def test_request_user_simple():
+    check_transformed(
+        """\
+        request.user.is_authenticated()
+        """,
+        """\
+        request.user.is_authenticated
+        """,
+        settings,
+    )
+
+
+def test_self_request_user_simple():
+    check_transformed(
+        """\
+        self.request.user.is_authenticated()
+        """,
+        """\
+        self.request.user.is_authenticated
+        """,
+        settings,
+    )
+
+
+def test_request_user_assigned():
+    check_transformed(
+        """\
+        auth = request.user.is_authenticated()
+        """,
+        """\
+        auth = request.user.is_authenticated
+        """,
+        settings,
+    )
+
+
+def test_self_request_user_assigned():
+    check_transformed(
+        """\
+        auth = self.request.user.is_authenticated()
+        """,
+        """\
+        auth = self.request.user.is_authenticated
+        """,
+        settings,
+    )
+
+
+def test_if_request_user():
+    check_transformed(
+        """\
+        if request.user.is_authenticated():
+            ... 
+        """,
+        """\
+        if request.user.is_authenticated:
+            ... 
+        """,
+        settings,
+    )
+
+
+def test_if_self_request_user():
+    check_transformed(
+        """\
+        if self.request.user.is_authenticated():
+            ... 
+        """,
+        """\
+        if self.request.user.is_authenticated:
+            ... 
+        """,
+        settings,
+    )
+
+
+def test_if_request_user_warlus():
+    check_transformed(
+        """\
+        if auth:= request.user.is_authenticated():
+            ... 
+        """,
+        """\
+        if auth:= request.user.is_authenticated:
+            ... 
+        """,
+        settings,
+    )
+
+
+def test_if_self_request_user_warlus():
+    check_transformed(
+        """\
+        if auth:= self.request.user.is_authenticated():
+            ... 
+        """,
+        """\
+        if auth:= self.request.user.is_authenticated:
+            ... 
+        """,
+        settings,
+    )
+
+
+def test_if_request_user_equal():
+    check_transformed(
+        """\
+        if request.user.is_authenticated() == True:
+            ... 
+        """,
+        """\
+        if request.user.is_authenticated == True:
+            ... 
+        """,
+        settings,
+    )
+
+
+def test_if_self_request_user_equal():
+    check_transformed(
+        """\
+        if request.user.is_authenticated() == True:
+            ... 
+        """,
+        """\
+        if request.user.is_authenticated == True:
+            ... 
+        """,
+        settings,
+    )

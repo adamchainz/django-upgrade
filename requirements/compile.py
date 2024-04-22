aa3bc191b332983d6aae5e3c16c73420f3887dd6
Adam Johnson <me@adamj.eu>
2024-04-22 14:09:43 +0100

    Use uv for requirements (#446)

--- requirements/compile.py
+++ requirements/compile.py
@@ -4,41 +4,23 @@ from __future__ import annotations
 import os
 import subprocess
 import sys
+from functools import partial
 from pathlib import Path
 
 if __name__ == "__main__":
     os.chdir(Path(__file__).parent)
-    os.environ["CUSTOM_COMPILE_COMMAND"] = "requirements/compile.py"
-    os.environ["PIP_REQUIRE_VIRTUALENV"] = "0"
     common_args = [
-        "-m",
-        "piptools",
+        "uv",
+        "pip",
         "compile",
+        "--quiet",
         "--generate-hashes",
-        "--allow-unsafe",
-    ] + sys.argv[1:]
-    subprocess.run(
-        ["python3.8", *common_args, "-o", "py38.txt"],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        ["python3.9", *common_args, "-o", "py39.txt"],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        ["python3.10", *common_args, "-o", "py310.txt"],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        ["python3.11", *common_args, "-o", "py311.txt"],
-        check=True,
-        capture_output=True,
-    )
-    subprocess.run(
-        ["python3.12", *common_args, "-o", "py312.txt"],
-        check=True,
-        capture_output=True,
-    )
+        "requirements.in",
+        *sys.argv[1:],
+    ]
+    run = partial(subprocess.run, check=True)
+    run([*common_args, "--python", "3.8", "--output-file", "py38.txt"])
+    run([*common_args, "--python", "3.9", "--output-file", "py39.txt"])
+    run([*common_args, "--python", "3.10", "--output-file", "py310.txt"])
+    run([*common_args, "--python", "3.11", "--output-file", "py311.txt"])
+    run([*common_args, "--python", "3.12", "--output-file", "py312.txt"])
